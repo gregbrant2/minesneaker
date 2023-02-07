32255790cfb0e7b7f303f1b63a28492f6a22ebd1
Greg Brant <greg@gregbrant.com>
2023-02-07 10:40:27 +0000

    Rough in a few types to model the problem

--- src/Minesneaker/Minesneaker.Tests/BoardTests.cs
+++ src/Minesneaker/Minesneaker.Tests/BoardTests.cs
@@ -1,5 +1,46 @@
+using Moq;
+
 namespace Minesneaker.Tests
 {
+    public class GameControllerTests
+    {
+        [Fact]
+        public void NewGame_Something()
+        {
+            var input = new Mock<IGameInput>();
+            var renderer = new Mock<IGameRenderer>();
+            var controller = new GameController(input.Object, renderer.Object);
+
+            controller.NewGame();
+        }
+    }
+
+    public class GameController
+    {
+        private readonly IGameInput _input;
+        private readonly IGameRenderer _renderer;
+        private Board? _board;
+
+        public GameController(IGameInput input, IGameRenderer renderer)
+        {
+            _input = input;
+            _renderer = renderer;
+        }
+
+        public void NewGame()
+        {
+            _board = new Board();
+        }
+    }
+
+    public interface IGameRenderer
+    {
+    }
+
+    public interface IGameInput
+    {
+    }
+
     public class BoardTests
     {
         [Fact]
