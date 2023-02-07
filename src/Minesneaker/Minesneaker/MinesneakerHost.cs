9749c51c81bc3e596f49b2f9ff7046c6b6c4a3d9
Greg Brant <greg@gregbrant.com>
2023-02-07 10:51:45 +0000

    Add DI and hosting plumbing

---
+++ src/Minesneaker/Minesneaker/MinesneakerHost.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+public class MinesneakerHost : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Hello, Minesneaker!");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
--- src/Minesneaker/Minesneaker/Program.cs
+++ src/Minesneaker/Minesneaker/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Minesneaker!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<MinesneakerHost>();
+    })
+    .Build();
+
+await host.RunAsync();
